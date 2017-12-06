fce7ee506cf6daab2d5a776c2e9a7bb4f7896c21
awells111 <awells111@gmail.com>
2017-12-06 17:10:26 -0500

    Test interface implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //todo initialize
+    }
+
+    public void actionSearchPart() {
+        System.out.println("searchPartClicked");
+    }
+
+    public void actionDeletePart() {
+        System.out.println("deletePartClicked");
+    }
+
+    public void actionModifyPart() {
+        System.out.println("modifyPartClicked");
+    }
+
+    public void actionAddPart() {
+        System.out.println("addPartClicked");
+    }
+
+    public void actionSearchProduct() {
+        System.out.println("searchProductClicked");
+    }
+
+    public void actionDeleteProduct() {
+        System.out.println("deleteProductClicked");
+    }
+
+    public void actionModifyProduct() {
+        System.out.println("modifyProductClicked");
+    }
+
+    public void actionAddProduct() {
+        System.out.println("addProductClicked");
+    }
+
+    public void actionExit() {
+        System.out.println("exitClicked");
+    }
 }
